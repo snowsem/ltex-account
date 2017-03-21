3ca209821caf21909237c2fe3b12c0e29579f72c
snowsem <snowsem@rambler.ru>
2017-03-21 20:51:51 +0300

    create migrations

---
+++ database/migrations/2017_03_21_173858_create_samples_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSamplesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('samples', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->text('text');
+            $table->string('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('samples');
+    }
+}
